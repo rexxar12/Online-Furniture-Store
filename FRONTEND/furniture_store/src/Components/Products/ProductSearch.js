f29348b1688d4140c92b30d3772a346148ca4646
rexxar12 <rbn1206@gmail.com>
2023-03-13 10:40:25 +0530

    99.99%

--- FRONTEND/furniture_store/src/Components/Products/ProductSearch.js
+++ FRONTEND/furniture_store/src/Components/Products/ProductSearch.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useLocation } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 import {
   MDBContainer,
   MDBRow,
@@ -11,25 +10,36 @@ import {
   MDBCardTitle,
 } from "mdb-react-ui-kit";
 import './Product.css';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+
+
+
 
 function ProductSearch() {
     const { state } = useLocation();
   const products = state.products || [];
   console.log(products);
+  const navigate = useNavigate();
+
+  const handleSearch = async (productName) => {
+    try {
+      let response = await axios.get(`http://localhost:8080/products/p/search?productName=${productName}`);
+      if (response.data) {
+        navigate(`/product-details?productName=${productName}`, { state: { products: response.data } });
+      } 
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <MDBContainer fluid className="my-5">
       <MDBRow className="justify-content-center">
       {products.map((product) => (
-          <Link
-          to={{
-            pathname: `/product-details`,
-            search: `?productName=${product.pname}`,
-            state: {
-              products: product.pname,
-            },
-          }}
-        >
         <MDBCol md="3">
+          <button className="btn bg-transparent" onClick={() => handleSearch(product.pname)}>
         <div key={product.id}>
           <MDBCard className="text-black">
           <MDBCardImage src={`data:image/png;base64,${product.productImage}`} position="top" alt={product.pname} />
@@ -57,8 +67,8 @@ function ProductSearch() {
           </MDBCardBody>
           </MDBCard>
         </div>
+        </button>
         </MDBCol>
-        </Link>
       ))}
       </MDBRow>
     </MDBContainer>
